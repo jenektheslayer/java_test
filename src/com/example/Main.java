bf334ebc9473c17d55e708749f856d35936d3338
Andrew <vip.andrusha2000@mail.ru>
2026-01-25 23:59:44 +0500

    исправлено

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,4 +1,12 @@
 package com.example;
 
+import static com.example.util.StringUtil.reverseLetters;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(reverseLetters("J@va the be$t!123"));
+    }
+
+
 }
+
--- src/com/example/util/StringUtil.java
+++ src/com/example/util/StringUtil.java
@@ -1,4 +1,26 @@
 package com.example.util;
 
 public class StringUtil {
+    public static String reverseLetters(String str) {
+        if (str == null || str.isEmpty()) {
+            return null;
+        }
+        char[] chars = str.toCharArray();
+
+        int left = 0;
+        int right = chars.length - 1;
+
+        while (left < right) {
+            if (Character.isLetter(chars[left])) {
+                if (Character.isLetter(chars[right])) {
+                    char temp = chars[left];
+                    chars[left] = chars[right];
+                    chars[right] = temp;
+                    left++;
+                }
+                right--;
+            } else left++;
+        }
+        return new String(chars);
+    }
 }
