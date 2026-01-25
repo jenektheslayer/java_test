56546b41ebf3955da4ef4766c04d05bac73aec7d
Andrew <vip.andrusha2000@mail.ru>
2026-01-26 02:23:05 +0500

    add println(2)

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -5,6 +5,7 @@ import static com.example.util.StringUtil.reverseLetters;
 public class Main {
     public static void main(String[] args) {
         System.out.println(reverseLetters("J@va the be$t!123"));
+        System.out.println(2);
     }
 
 
