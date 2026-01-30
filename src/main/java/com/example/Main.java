093cc2996a1f0cd3ddca4608ecea93a6b8c5b68e
Andrew <vip.andrusha2000@mail.ru>
2026-01-30 12:26:05 +0500

    Tests Done

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,7 +5,6 @@ import static com.example.util.StringUtil.reverseLetters;
 public class Main {
     public static void main(String[] args) {
         System.out.println(reverseLetters("J@va the be$t!123"));
-        System.out.println(2);
     }
 
 
--- src/test/java/com/example/util/StringUtilTest.java
+++ src/test/java/com/example/util/StringUtilTest.java
@@ -11,22 +11,27 @@ public class StringUtilTest {
         String str = StringUtil.reverseLetters("String");
         assertEquals("gnirtS", str);
     }
+    @Test
     public void reverseLetters_shouldReturnEmpty_ifContainsNothing() {
         String str = StringUtil.reverseLetters("");
-        assertEquals("", str);
+        assertEquals(null, str);
     }
+    @Test
     public void reverseLetters_shouldReturnStringAndNumbers() {
         String str = StringUtil.reverseLetters("1a2b3");
         assertEquals("1b2a3", str);
     }
+    @Test
     public void reverseLetters_shouldReturnNull() {
         String str = StringUtil.reverseLetters(null);
         assertNull(str);
     }
+    @Test
     public void reverseLetters_shouldReturnString_ifContainsSpace() {
         String str = StringUtil.reverseLetters("a b c d e f g");
         assertEquals("g f e d c b a",str);
     }
+    @Test
     public void reverseLetters_shouldReturnNumbers_ifContainsOnlyNumbers() {
         String str = StringUtil.reverseLetters("123");
         assertEquals("123",str);
