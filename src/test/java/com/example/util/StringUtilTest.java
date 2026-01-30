07f9bea26c8089d24dd26217c51f0cf2d561808b
Andrew <vip.andrusha2000@mail.ru>
2026-01-30 11:25:14 +0500

    Tests Done

---
+++ src/test/java/com/example/util/StringUtilTest.java
@@ -0,0 +1,37 @@
+package com.example.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class StringUtilTest {
+    @Test
+    public void reverseLetters_shouldReturnString_ifContainsOnlyLetters() {
+        String str = StringUtil.reverseLetters("String");
+        assertEquals("gnirtS", str);
+    }
+    public void reverseLetters_shouldReturnEmpty_ifContainsNothing() {
+        String str = StringUtil.reverseLetters("");
+        assertEquals("", str);
+    }
+    public void reverseLetters_shouldReturnStringAndNumbers() {
+        String str = StringUtil.reverseLetters("1a2b3");
+        assertEquals("1b2a3", str);
+    }
+    public void reverseLetters_shouldReturnNull() {
+        String str = StringUtil.reverseLetters(null);
+        assertNull(str);
+    }
+    public void reverseLetters_shouldReturnString_ifContainsSpace() {
+        String str = StringUtil.reverseLetters("a b c d e f g");
+        assertEquals("g f e d c b a",str);
+    }
+    public void reverseLetters_shouldReturnNumbers_ifContainsOnlyNumbers() {
+        String str = StringUtil.reverseLetters("123");
+        assertEquals("123",str);
+    }
+
+}
+
+
