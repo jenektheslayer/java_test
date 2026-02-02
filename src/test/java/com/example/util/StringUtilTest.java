cced72751fc547e7e7d475d7c5ed75a81d94f6de
Andrew <vip.andrusha2000@mail.ru>
2026-02-02 05:35:54 +0500

    deleted target/ created space between methods

--- src/test/java/com/example/util/StringUtilTest.java
+++ src/test/java/com/example/util/StringUtilTest.java
@@ -11,16 +11,19 @@ public class StringUtilTest {
         String str = StringUtil.reverseLetters("String");
         assertEquals("gnirtS", str);
     }
+
     @Test
     public void reverseLetters_shouldReturnEmpty_ifContainsNothing() {
         String str = StringUtil.reverseLetters("");
         assertEquals(null, str);
     }
+
     @Test
     public void reverseLetters_shouldReturnStringAndNumbers() {
         String str = StringUtil.reverseLetters("1a2b3");
         assertEquals("1b2a3", str);
     }
+
     @Test
     public void reverseLetters_shouldReturnNull() {
         String str = StringUtil.reverseLetters(null);
